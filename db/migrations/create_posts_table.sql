a523f3f4719b01c10709ef24890051cefdf580ea
Will Wayman <will.jdw@gmail.com>
2019-01-10 10:51:03 +0000

    added sql to create databases and users, posts tables

---
+++ db/migrations/create_posts_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  time_stamp TIMESTAMP DEFAULT NOW(),
+  text varchar(140) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
